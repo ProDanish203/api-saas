ad5f162198bb8028454cf21d6b9fa2464e362b91
Danish <danishsidd524@gmail.com>
2025-02-20 21:04:58 +0500

    add: sidebar and stripe

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,7 +3,7 @@ import { UserProfileIcon } from "../helper";
 
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between md:px-6 px-2 py-6 h-[60px] container">
+    <header className="bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground flex items-center justify-between md:px-6 px-4 py-6 h-[60px] w-full">
       <div></div>
       <UserProfileIcon />
     </header>
